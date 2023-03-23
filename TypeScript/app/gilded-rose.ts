ec7fd2c5ae2a4c68b509f7c2c4c36a2ecfa19f24
Josu Vazquez <josu.vazquez@scalefast.com>
2023-03-23 09:05:33 +0100

    refactor: naming refactor

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -1,14 +1,15 @@
-export class Item {
-  name: string;
-  sellIn: number;
-  quality: number;
+import { Item } from "./item.model";
 
-  constructor(name, sellIn, quality) {
-    this.name = name;
-    this.sellIn = sellIn;
-    this.quality = quality;
-  }
-}
+const AGED_BRIE = "Aged Brie";
+const BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+const SULFURAS = "Sulfuras, Hand of Ragnaros";
+const AGED_BRIE_DOUBLE_QUALITY_DECREMENT_SELL_IN_THRESHOLD = 0;
+const BACKSTAGE_PASSES_DOUBLE_QUALITY_INCREASE_SELL_IN_THRESHOLD = 10;
+const BACKSTAGE_PASSES_TRIPLE_QUALITY_INCREASE_SELL_IN_THRESHOLD = 5;
+const BACKSTAGE_PASSES_QUALITY_RESET_SELL_IN_THRESHOLD = 0;
+const DEFAULT_ITEM_DOUBLE_QUALITY_DECREASE_SELL_IN_THRESHOLD = 0;
+const MAX_QUALITY = 50;
+const MIN_QUALITY = 0;
 
 export class GildedRose {
   items: Array<Item>;
@@ -18,52 +19,79 @@ export class GildedRose {
   }
 
   updateQuality() {
-    for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-            }
-          }
-        }
-      }
-      if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality
-          }
-        } else {
-          if (this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1
-          }
-        }
+    for (let item of this.items) {
+      switch (item.name) {
+        case AGED_BRIE:
+          this.decreaseSellIn(item);
+          this.updateAgedBrieQuality(item);
+          break;
+        case BACKSTAGE_PASSES:
+          this.decreaseSellIn(item);
+          this.updateBackstagePassesQuality(item);
+          break;
+        case SULFURAS:
+          break;
+        default:
+          this.decreaseSellIn(item);
+          this.updateDefaultItemQuality(item);
+          break;
       }
     }
+  }
 
-    return this.items;
+  decreaseSellIn(item: Item) {
+    item.sellIn -= 1;
+  }
+
+  updateAgedBrieQuality(item: Item) {
+    this.increaseQuality(item);
+
+    if (item.sellIn < AGED_BRIE_DOUBLE_QUALITY_DECREMENT_SELL_IN_THRESHOLD) {
+      this.increaseQuality(item);
+    }
+  }
+
+  updateBackstagePassesQuality(item: Item) {
+    this.increaseQuality(item);
+
+    if (
+      item.sellIn < BACKSTAGE_PASSES_DOUBLE_QUALITY_INCREASE_SELL_IN_THRESHOLD
+    ) {
+      this.increaseQuality(item);
+    }
+
+    if (
+      item.sellIn < BACKSTAGE_PASSES_TRIPLE_QUALITY_INCREASE_SELL_IN_THRESHOLD
+    ) {
+      this.increaseQuality(item);
+    }
+
+    if (item.sellIn < BACKSTAGE_PASSES_QUALITY_RESET_SELL_IN_THRESHOLD) {
+      this.resetQuality(item);
+    }
+  }
+
+  updateDefaultItemQuality(item: Item) {
+    this.decreaseQuality(item);
+
+    if (item.sellIn < DEFAULT_ITEM_DOUBLE_QUALITY_DECREASE_SELL_IN_THRESHOLD) {
+      this.decreaseQuality(item);
+    }
+  }
+
+  resetQuality(item: Item) {
+    item.quality = 0;
+  }
+
+  increaseQuality(item: Item) {
+    if (item.quality < MAX_QUALITY) {
+      item.quality += 1;
+    }
+  }
+
+  decreaseQuality(item: Item) {
+    if (item.quality > MIN_QUALITY) {
+      item.quality -= 1;
+    }
   }
 }
